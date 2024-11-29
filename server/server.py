99d0fcae489c4fdcb1fe7c3928eb3672e1a66bd7
SK <limsweekiat@gmail.com>
2024-11-29 14:47:54 +0800

    Switch port to 8080

--- server/server.py
+++ server/server.py
@@ -3,4 +3,4 @@ import uvicorn
 
 def start():
     """Launched with `poetry run start` at root level"""
-    uvicorn.run("server.main:app", host="0.0.0.0", port=80, reload=True)
+    uvicorn.run("server.main:app", host="0.0.0.0", port=8080, reload=True)
